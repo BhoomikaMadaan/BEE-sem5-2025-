af4e4c8bb07321c2cd42c8b0e8328c4bdf3ad64b
BhoomikaMadaan <bhoomika4770.be23@chitkara.edu.in>
2025-07-24 20:29:03 +0530

    Delete L5/index.js

--- L5/index.js
+++
@@ -1,5 +0,0 @@
-import add from  "./math.js";
-import {sub,mul} from "./math.js"; //while  using the named export , u have to write the function in between the curly braces 
-console.log(sub);//named export
-console.log(mul);
-console.log(add);//default export
