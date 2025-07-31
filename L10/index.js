22bead54edf977be48a2f7c30d28f2e1a05c4ae2
Bhoomika Madaan <bhoomika4770.be23@chitkara.edu.in>
2025-07-31 10:49:46 +0530

    Add files via upload

--- L10/index.js
+++ L10/index.js
@@ -1,45 +1,11 @@
 
-const fs=require("fs")
+const express=require ('express')
+const app = express()
 
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
-let  users=[
-{
-    id:1,
-    name:"kkubk",
-    email:"abcd@gmail.com",
-    address:"Patiala",
-    password:"2345"
-},
-{
-    id:1,
-    name:"Harshik",
-    email:"edf@gmail.com",
-    address:"Patiala",
-    password:"2345"
-},
-{
-    id:1,
-    name:"ziw",
-    email:"idf@gmail.com",
-    address:"Patiala",
-    password:"2345"
-}
-
-]
-
-
-// fs.writeFile("./data.json",JSON.stringify(users),function(err)
-// {
-//     if(err) return err;
-//       console.log("completed");
-
-
-// })
-
-fs.readFile("write/data.json","utf-8",function(err,data){
-    if(err) return err;
-    // console.log(users);
-
-    let users=JSON.parse(data);
-    console.log(users[1]);
+app.listen(3000,()=>{
+    console.log("server started...");
 })
